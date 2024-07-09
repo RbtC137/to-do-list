d31d9273e9f370286457b0d4c724befa7ac7fbd0
RbtC137 <rbtcc137@outlook.com>
2024-07-09 23:41:45 +0800

    Refactoring light/dark mode

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const ldBar = document.querySelector(".bar");
+const ldBar = document.querySelector(".bar-js");
 const body = document.querySelector("body");
 let modeFlag = false;
 
@@ -6,13 +6,8 @@ ldBar.addEventListener("click",ldModeChange);
 
 
 function ldModeChange() {
-    if(modeFlag){
-        ldBar.setAttribute("class","bar lm-on");
-        body.setAttribute("class","");
-        modeFlag = false;
-    }else {
-        ldBar.setAttribute("class","bar dm-on");
-        body.setAttribute("class","dark-mode");
-        modeFlag = true;
-    }
+    ldBar.classList.toggle("dm-on");
+    body.classList.toggle("dark-mode");
+    modeFlag = false;
+ 
 }
